ca55dd2b8a695f57eb568877bad62c1b07e7d4eb
ALEXANDER <msumba83@gmail.com>
2021-07-03 22:37:18 +0200

    variable

--- variables.js
+++ variables.js
@@ -1,7 +1,16 @@
+// VARIABLES
+
 let x = 800;
-let y = 100;
+let y = 1008;
 let z = x * y;
 console.log("Results: " + z);
+
+
 console.log('Alexander is studying JavaScript');
+
+let fistNumber = 5;
+let secondNumber =10;
+let thirdNumber = fistNumber + secondNumber;
+console.log("SUM:", + thirdNumber);
 
 
