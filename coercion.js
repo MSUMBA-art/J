08fa319788024fbf4aec6c9be0d751e95e061974
ALEXANDER <msumba83@gmail.com>
2021-07-04 22:16:14 +0200

    Update coercion.js

--- coercion.js
+++ coercion.js
@@ -1,10 +1,10 @@
 let a = 10;
-let b = '6';
+let b = "6";
 b = parseInt(b, 10);
 let c = a + b;
-console.log('Answer is ' + c);
+console.log("Answer is " + c);
 
-let d = parseInt('bob', 10);
+let d = parseInt("bob", 10);
 let e = isNaN(d);
 console.log(d);
 console.log(e);
