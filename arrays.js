323ca0ac2d18f0d8c4d692d991eaa1e5f449a153
ALEXANDER <msumba83@gmail.com>
2021-07-10 07:30:23 +0200

    done operators

---
+++ arrays.js
@@ -0,0 +1,4 @@
+let a = [4, 12, 788, 9, 7666];
+let b = [ 'David', 'Alex', 'Bertha', 'Irene'];
+
+console.log(a);
--- operators.js
+++ operators.js
@@ -1,4 +1,31 @@
 // CATEGORIES OF OPERATORS
 // 1 . assignment operator =
-// 2. arithmetic operator +, -, *, /
-      increment / decrement ++, --
+// 2. arithmetic operator +, -, *, /, %
+// var m = 10 % 3;
+// console.log(m);
+// //increment / decrement ++, --
+// var a = 1;
+// a++; //this will increase number by 1
+// console.log(a++);
+// console.log(a);
+
+//STRING '' +
+
+//Presedence
+//var b = (1 + 1) * 3; // this will give 6
+// var c = 1 + 1 * 3; // this will give 4
+// console.log(b);
+// console.log(c);
+
+// function invocation operators ()
+// console.log()
+
+// logical and: && or: ||
+
+// Member accessor operators
+// console.log()
+
+// code block operators
+
+// Array element access operators
+
