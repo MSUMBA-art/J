5e056790d80f170382a4679d0bff6ec6fc619008
ALEXANDER <msumba83@gmail.com>
2021-07-18 13:36:02 +0200

    done arrays

--- arrays.js
+++ arrays.js
@@ -1,14 +1,33 @@
-let a = [4, 12, 788, 9, 7666];
+let a = [4, 12, 788, 9, 76];
 let b = [ 'David', 'Alex', 'Bertha', 'Irene'];
 
-// console.log(a[0]);
-// console.log(a[1]);
+//  console.log(a[3]);
+//  console.log(a[1]);
+//  console.log(b);
+
+//  a[4] = 10; //sign a new value
 // console.log(a);
 
-/* a[0] = 6000; //sign a new value
+//console.log(typeof b, typeof a);
+
+
+//let c = [4, "alex", true];
+//console.log(c)
+
+//console.log(b[4])
+
+// a[10] = 77;
+// console.log(a)
+// console.log(a.length)
+
+// console.log(a.length) // it will give us actual number NOT zero based
+a.push(1); //push method adds an Object
 console.log(a);
+console.log(a.length); //.length method calculate number of objects
+a.pop(); // pop method removes an object from array
+a.pop();
+a.pop();
+a.pop();
+console.log(a);
+console.log(a.length);
 
-console.log(typeof a);
-*/
-
-let c = []
