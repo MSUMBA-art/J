a449f24f885dab2e702e8137792411d95961695e
ALEXANDER <msumba83@gmail.com>
2021-07-18 14:00:56 +0200

    started function declarations

---
+++ function-declaration.js
@@ -0,0 +1,24 @@
+// function sayHello() {
+//     console.log('......................');
+//     console.log('Hello!');
+//     console.log('............'); 
+// }
+//the above is what we call function declation
+
+//INVOKE A Function
+//sayHello(); 
+
+// let a = sayHello;
+// a();
+// a();
+
+function sayHello(name) {
+    console.log('......................');
+    console.log('Hello: ' + name + '!');
+    console.log('............'); 
+}
+
+sayHello("alex");
+sayHello("bertha");
+sayHello("irene Msumba");
+
