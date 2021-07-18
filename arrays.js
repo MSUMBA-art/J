05453781354a1f4deb516af22dc40f0d1a0e8842
ALEXANDER <msumba83@gmail.com>
2021-07-18 12:38:26 +0200

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,4 +1,14 @@
 let a = [4, 12, 788, 9, 7666];
 let b = [ 'David', 'Alex', 'Bertha', 'Irene'];
 
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a);
+
+/* a[0] = 6000; //sign a new value
 console.log(a);
+
+console.log(typeof a);
+*/
+
+let c = []
