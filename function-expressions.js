7316ea4e6be4e6cd3b5703a45957c867a719fc4e
ALEXANDER <msumba83@gmail.com>
2021-07-18 20:00:16 +0200

    done expressions

---
+++ function-expressions.js
@@ -0,0 +1,16 @@
+// setTimeout(function(){
+//     console.log('I visited 2 seconds');
+// }, 2000);
+
+// let counter = 0;
+// function timeout() {
+//     setTimeout(function () {
+//         console.log('Alexander is studying web dev ' + counter++);
+//         timeout()
+//     }, 2000); 
+// }
+// timeout();
+
+(function () {
+    console.log('alex');
+})();
