aa6da1a6086387e5782f3ff5b6265107820356a1
ALEXANDER <msumba83@gmail.com>
2021-07-19 06:51:47 +0200

    Create decission.js

---
+++ decission.js
@@ -0,0 +1,26 @@
+//if statements
+// var count = 3;
+// if (count == 4) {
+//   console.log('count is 4');
+// } else if(count > 4) {
+//     console.log('count is greater than 4');
+// }else if (count < 4) {
+//     console.log('count is less than 4');
+// } else {
+//    console.log("count is NOT 4"); 
+// }
+
+//switch statement
+let hero ='Alex';
+switch (hero) {
+    case 'Alex':
+        console.log('super strenght');
+        console.log('x-ray vision');
+    case 'car':
+        break;
+        console.log('clever');
+        console.log('good');
+    default:
+        console.log('from MalAWI');
+
+}
