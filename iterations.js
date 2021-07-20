0a4d91c4cf824ffa7ef03a7b5bf640ade8179133
ALEXANDER <msumba83@gmail.com>
2021-07-20 19:22:23 +0200

    Update iterations.js

--- iterations.js
+++ iterations.js
@@ -7,10 +7,24 @@
 //     const element = array[index];
     
 // }
-function name(n) {
-    let a = [4, 8, 15, 17, 18, 48];
-    for (i = 0; i < a.length; i++) {
-      console.log(a[i]);
-    }
-       
-}
+// function name(n) {
+//     let a = [4, 8, 15, 17, 18, 48];
+//     for (i = 0; i < a.length; i++) {
+//       console.log(a[i]);
+//     }      
+// }
+
+// for loop
+/*let a = ['ale : 12', 'ber', 15, 17, 18, 48];
+for (let b = 0; b < a.length; b++) {
+  const c = a[b];
+  console.log(c);
+  }
+  */
+
+  let x = 1;
+  while (x < 10) {
+    console.log(x++);
+  }
+  
+
