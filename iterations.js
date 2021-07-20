8096254112ec6b155492fa1cc092e4264a4c1bb0
ALEXANDER <msumba83@gmail.com>
2021-07-20 06:54:52 +0200

    Create iterations.js

---
+++ iterations.js
@@ -0,0 +1,16 @@
+// iterations statements
+// for ( i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+function name(n) {
+    let a = [4, 8, 15, 17, 18, 48];
+    for (i = 0; i < a.length; i++) {
+      console.log(a[i]);
+    }
+       
+}
