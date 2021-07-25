3352984519fcb9e2d7d1bcb0493ab081277f4bd0
ALEXANDER <msumba83@gmail.com>
2021-07-25 10:05:17 +0200

    done scopes and started functions

---
+++ returning-functions.js
@@ -0,0 +1,16 @@
+//DONT DO THIS
+/*var myVariable = 'I am at the global scope';
+
+function myVariable() {
+    console.log(myVariable);
+}
+*/
+
+// do this
+// RETURNING A FUNCTION
+function one() {
+    return 'one';
+}
+
+//let value = one()
+//console.log(value);
