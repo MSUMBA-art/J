bcd2100517aa1230edd0e2bc705d4cdc4e2fdb24
ALEXANDER <msumba83@gmail.com>
2021-07-25 10:49:56 +0200

    done returning functions

--- returning-functions.js
+++ returning-functions.js
@@ -8,9 +8,45 @@ function myVariable() {
 
 // do this
 // RETURNING A FUNCTION
-function one() {
-    return 'one';
-}
+
+
+
+/*function one() {
+    return 'alex'
+}*/
 
 //let value = one()
 //console.log(value);
+
+//console.log(one()); // this works same as the above two lines
+
+//let value = one;
+//console.log(typeof value);
+//console.log(typeof one);
+//console.log(value()); // invoke ()
+
+//types of values
+/* 1 string
+   2 number
+   3 boolean
+   4 undefined
+   5 function
+   */
+
+   /*function two() {
+       return function() {
+           console.log('two');
+       }
+   }
+
+   let myFunction = two();
+   myFunction();
+   */
+
+   function three() {
+       return function() {
+           return 'three'
+       }
+   }
+
+   console.log(three()())
